2b1e5416491d970c68a55f65ec31f71145cdab81
Finteckinfo <fintechinfo.org@gmail.com>
2025-08-13 18:19:07 +0300

    sso-callback

--- src/router/PublicRoutes.ts
+++ src/router/PublicRoutes.ts
@@ -24,6 +24,11 @@ const PublicRoutes = {
       name: 'Error 404',
       path: '/error',
       component: () => import('@/views/pages/maintenance/error/Error404Page.vue')
+    },
+    {
+      name: 'SSO - callback',
+      path: '/sso-callback',
+      component: () => import('@/views/authentication/SSOCallback.vue')
     }
   ]
 };
